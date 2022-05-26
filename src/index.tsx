5980f8f41bbbca51e65ae7a568b0cf833f33c841
Alexander <braylore27@gmail.com>
2022-05-26 17:01:42 +0300

    index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,51 @@
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
+import { store } from './store/store';
+
+const theme = createTheme({
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true
+      }
+    },
+    MuiBreadcrumbs: {
+      styleOverrides: {
+        separator: {
+          color: '#f5c524'
+        }
+      }
+    },
+    MuiCardActionArea: {
+      styleOverrides: {
+        focusHighlight: {
+          opacity: 0.3
+        }
+      }
+    }
+  },
+  palette: {
+    background: {
+      paper: '#243e27'
+    }
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <CssBaseline>
-    <App />
-  </CssBaseline>
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </CssBaseline>
+    </ThemeProvider>
+  </BrowserRouter>
 );
 
