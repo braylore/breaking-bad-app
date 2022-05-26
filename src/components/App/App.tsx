07b45696f521add8da8523b34315de0e5fa1666b
Alexander <braylore27@gmail.com>
2022-05-26 17:02:58 +0300

    App routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,36 @@
+import { Routes, Route } from 'react-router-dom';
+import AboutPage from '../../pages/AboutPage/AboutPage';
+import CharactersPage from '../../pages/CharactersPage/CharactersPage';
+import EpisodesPage from '../../pages/EpisodesPage/EpisodesPage';
+import GalleryPage from '../../pages/GalleryPage/GalleryPage';
+import Layout from '../Layout/Layout';
+
 const App = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<Layout />}
+      >
+        <Route
+          index
+          element={<AboutPage />}
+        />
+        <Route
+          path="Characters"
+          element={<CharactersPage />}
+        />
+        <Route
+          path="Episodes"
+          element={<EpisodesPage />}
+        />
+        <Route
+          path="Gallery"
+          element={<GalleryPage />}
+        />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
