dddd8460dca106408f793abbe57dc588d6a75267
Alexander <braylore27@gmail.com>
2022-05-26 16:56:38 +0300

    Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,32 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '../UI/Breadcrumbs/Breadcrumbs';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Box>
+      <AppBar
+        elevation={8}
+        sx={{ p: 1, backgroundColor: '#191e19' }}
+        position="static"
+      >
+        <Toolbar>
+          <Typography
+            sx={{
+              mr: '25px'
+            }}
+            variant="h6"
+            component="div"
+          >
+            Breaking Bad App
+          </Typography>
+          <Breadcrumbs />
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Header;
