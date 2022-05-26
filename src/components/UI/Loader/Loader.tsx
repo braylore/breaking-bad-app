2fc223661fc3198476b2303c16f53ce74194a64a
Alexander <braylore27@gmail.com>
2022-05-26 16:58:08 +0300

    Loader

---
+++ src/components/UI/Loader/Loader.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './loader.module.scss';
+
+const Loader = () => {
+  return (
+    <div className={styles.spinner}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
