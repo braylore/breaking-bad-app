8bcafcded0eb08f645eeee4f93165b3b54e6f8e4
Alexander <braylore27@gmail.com>
2022-05-29 17:08:56 +0300

    index.tsx changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme, ThemeProvider } from '@mui/material';
+import { ThemeProvider } from '@mui/material';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 import { store } from './store/store';
-
-const theme = createTheme({
-  components: {
-    MuiButtonBase: {
-      defaultProps: {
-        disableRipple: true
-      }
-    },
-    MuiBreadcrumbs: {
-      styleOverrides: {
-        separator: {
-          color: '#f5c524'
-        }
-      }
-    },
-    MuiCardActionArea: {
-      styleOverrides: {
-        focusHighlight: {
-          opacity: 0.3
-        }
-      }
-    }
-  },
-  palette: {
-    background: {
-      paper: '#243e27'
-    }
-  }
-});
+import { theme } from './theme/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
