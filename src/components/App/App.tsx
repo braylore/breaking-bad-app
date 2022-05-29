3300e5540b4611f397dbf4eb0dad668b446b8008
Alexander <braylore27@gmail.com>
2022-05-29 17:22:30 +0300

    added more routes to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,7 @@ import AboutPage from '../../pages/AboutPage/AboutPage';
 import CharactersPage from '../../pages/CharactersPage/CharactersPage';
 import EpisodesPage from '../../pages/EpisodesPage/EpisodesPage';
 import GalleryPage from '../../pages/GalleryPage/GalleryPage';
+import SingleCharacterPage from '../../pages/SingleCharacterPage/SingleCharacterPage';
 import Layout from '../Layout/Layout';
 
 const App = () => {
@@ -19,6 +20,10 @@ const App = () => {
         <Route
           path="Characters"
           element={<CharactersPage />}
+        />
+        <Route
+          path="Characters/:name"
+          element={<SingleCharacterPage />}
         />
         <Route
           path="Episodes"
