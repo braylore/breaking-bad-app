0ba5f159ccc9f87c2d47053761ae6870d1322fd3
Alexander <braylore27@gmail.com>
2022-05-29 17:20:17 +0300

    CharactersPage

--- src/pages/CharactersPage/CharactersPage.tsx
+++ src/pages/CharactersPage/CharactersPage.tsx
@@ -1,7 +1,63 @@
+import { ChangeEvent, useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { fetchCharacters } from '../../store/actions/actions';
+import ToggleBtns from '../../components/UI/ToggleBtns/ToggleBtns';
+import Loader from '../../components/UI/Loader/Loader';
+import { ICharacters } from '../../types/ICharacters';
+import Search from '../../components/UI/Search/Search';
+import CharactersList from '../../components/CharactersList/CharactersList';
+import mainStyles from '../../styles/main.module.scss';
 
 const CharactersPage = () => {
-  return <div>CharactersPage</div>;
+  const dispatch = useAppDispatch();
+  const { characters, error, isLoading } = useAppSelector((state) => state.charactersReducer);
+  const [query, setQuery] = useState('');
+
+  const handleQueryChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    dispatch(fetchCharacters());
+  }, []);
+
+  const handleClick = (value: string) => {
+    dispatch(fetchCharacters(value));
+  };
+
+  const searchChar = (chars: ICharacters[], searchQuery: string) => {
+    if (Array.isArray(chars)) {
+      return chars.filter((c) => c.name.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase()));
+    }
+    return [];
+  };
+
+  const charList = searchChar(characters, query);
+
+  if (error || !Array.isArray(characters)) {
+    return <h2 className={mainStyles.msg}>An error occurred while loading characters. Please try again later.</h2>;
+  }
+
+  return (
+    <>
+      <ToggleBtns
+        isDisabled={isLoading}
+        handleClick={handleClick}
+      />
+      <Search
+        isDisabled={isLoading}
+        query={query}
+        handleQueryChange={handleQueryChange}
+      />
+      {isLoading ? (
+        <Loader />
+      ) : charList.length ? (
+        <CharactersList charList={charList} />
+      ) : (
+        <h2 className={mainStyles.msg}>No characters found.</h2>
+      )}
+    </>
+  );
 };
 
 export default CharactersPage;
