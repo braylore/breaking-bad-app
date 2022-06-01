ce6b9bcd2b0950a916e4615b78801468f577ff29
Alexander <braylore27@gmail.com>
2022-06-01 17:09:59 +0300

    removed unnecessary files

--- src/utils/getSlicedPosters.ts
+++
@@ -1,5 +0,0 @@
-import { IPosterParams } from '../types/IPosters';
-
-export const getSlicedPosters = (currentSlice: number, posters: IPosterParams[]): IPosterParams[] => {
-  return posters.slice(18 * currentSlice - 18, 18 * currentSlice);
-};
