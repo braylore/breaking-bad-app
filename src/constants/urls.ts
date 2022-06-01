46f252143f38f31d0b4cc2112fdcdd4cfd546ff4
Alexander <braylore27@gmail.com>
2022-06-01 16:52:49 +0300

    added more urls const

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,4 +1,5 @@
 export const BASE_URL_IMDB = 'https://imdb-api.com/en/API/';
-export const SERIES_ID_IMDB = 'tt0903747';
-export const API_KEY = process.env.REACT_APP_API_KEY;
+export const BB_ID_IMDB = 'tt0903747';
+export const BCS_ID_IMDB = 'tt3032476';
 export const BASE_URL_BBAPI = 'https://www.breakingbadapi.com/api/';
+export const API_KEY = process.env.REACT_APP_API_KEY;
