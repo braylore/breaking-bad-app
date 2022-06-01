9d64271c915bf5531a6db9b5cfd4be853ab7438a
Alexander <braylore27@gmail.com>
2022-06-01 16:59:33 +0300

    logo and styles to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,41 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import { IconButton } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import bbLogo from '../../assets/img/bbLogo.png';
 import Breadcrumbs from '../UI/Breadcrumbs/Breadcrumbs';
+import styles from './header.module.scss';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
+
   return (
     <Box>
       <AppBar
         elevation={8}
-        sx={{ p: 1, backgroundColor: '#191e19' }}
+        sx={{ backgroundColor: '#191e19' }}
         position="static"
       >
         <Toolbar>
-          <Typography
+          <IconButton
+            onClick={handleClick}
             sx={{
-              mr: '25px'
+              width: '110px',
+              height: '80px',
+              mr: '10px'
             }}
-            variant="h6"
-            component="div"
           >
-            Breaking Bad App
-          </Typography>
+            <img
+              className={styles.logo}
+              src={bbLogo}
+              alt="Breaking Bad logo"
+            />
+          </IconButton>
           <Breadcrumbs />
         </Toolbar>
       </AppBar>
