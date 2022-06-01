454954a587d8ba64d91526098d7f64130733bed6
Alexander <braylore27@gmail.com>
2022-06-01 17:00:29 +0300

    added async actions

--- src/store/actions/actions.ts
+++ src/store/actions/actions.ts
@@ -1,11 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { API_KEY, BASE_URL_BBAPI, BASE_URL_IMDB, SERIES_ID_IMDB } from '../../constants/urls';
+import { API_KEY, BASE_URL_BBAPI, BASE_URL_IMDB, BB_ID_IMDB } from '../../constants/urls';
 import { ICharacters } from '../../types/ICharacters';
 import { IPosters } from '../../types/IPosters';
 import { IQuote } from '../../types/IQuote';
+import { IEpisodes } from '../../types/IEpisodes';
+import { IEpisodesRespIMDB } from '../../types/IEpisodesIMDB';
 
-export const fetchCharacters = createAsyncThunk('characters/fetchAll', async (category: string | void, thunkAPI) => {
+export const fetchCharacters = createAsyncThunk('characters/fetchAll', async (category: string | null, thunkAPI) => {
   try {
     const response = await axios.get<ICharacters[]>(`${BASE_URL_BBAPI}characters`, {
       params: {
@@ -33,7 +35,7 @@ export const fetchCharacterByName = createAsyncThunk('characters/fetchByName', a
 
 export const fetchPosters = createAsyncThunk('posters/fetchAll', async (_, thunkAPI) => {
   try {
-    const response = await axios.get<IPosters>(`${BASE_URL_IMDB}Posters/${API_KEY}/${SERIES_ID_IMDB}`);
+    const response = await axios.get<IPosters>(`${BASE_URL_IMDB}Posters/${API_KEY}/${BB_ID_IMDB}`);
     return response.data;
   } catch (e) {
     return thunkAPI.rejectWithValue('An error occurred while loading posters');
@@ -52,3 +54,40 @@ export const fetchRandomQuote = createAsyncThunk('quote/fetchRandomByAuthor', as
     return thunkAPI.rejectWithValue('An error occurred while loading quote');
   }
 });
+
+export const fetchEpisodes = createAsyncThunk('episodes/fetchAllOrById', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get<IEpisodes[]>(`${BASE_URL_BBAPI}episodes`);
+    return response.data;
+  } catch (e) {
+    return thunkAPI.rejectWithValue('An error occurred while loading episodes');
+  }
+});
+
+export const fetchEpisodeById = createAsyncThunk('episodes/fetchById', async (episodeId: string, thunkAPI) => {
+  try {
+    const response = await axios.get<IEpisodes[]>(`${BASE_URL_BBAPI}episodes/${episodeId}`);
+    return response.data;
+  } catch (e) {
+    return thunkAPI.rejectWithValue('An error occurred while loading episodes');
+  }
+});
+
+interface IFetchSpisodesIMDBParams {
+  seriesId: string;
+  seasonNum: string;
+}
+
+export const fetchEpisodesIMDB = createAsyncThunk(
+  'episodesIMDB/fetchSpecified',
+  async ({ seriesId, seasonNum }: IFetchSpisodesIMDBParams, thunkAPI) => {
+    try {
+      const response = await axios.get<IEpisodesRespIMDB>(
+        `${BASE_URL_IMDB}SeasonEpisodes/${API_KEY}/${seriesId}/${seasonNum}`
+      );
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue('An error occurred while loading episodes');
+    }
+  }
+);
